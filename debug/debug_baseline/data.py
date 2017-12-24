44cba24cb0473f5fffdd9dd3eb9eb7d8523d632e
zbwglory <zhan734@usc.edu>
2017-12-24 12:24:18 -0800

    first push

---
+++ debug/debug_baseline/data.py
@@ -0,0 +1,122 @@
+import torch
+import torch.utils.data as data
+import torchvision.transforms as transforms
+import os
+import nltk
+from PIL import Image
+from pycocotools.coco import COCO
+import numpy as np
+import json as jsonmod
+import h5py
+
+class PrecompDataset(data.Dataset):
+    """
+    Load precomputed captions and image features
+    Possible options: f8k, f30k, coco, 10crop
+    """
+
+    def __init__(self, data_path, data_split, vocab):
+        self.vocab = vocab
+        loc = '/data2/bwzhang/anet_img/captions/'
+
+        # Captions
+        self.jsondict = jsonmod.load(open(loc+'{}.json'.format(data_split), 'r'))
+        self.ann_id = {}
+        for i, keys in enumerate(self.jsondict.keys()):
+            self.ann_id[i] = keys
+
+        # Image features
+        self.video_emb = h5py.File('sub_activitynet_v1-3.c3d.hdf5')
+
+        self.length = len(self.ann_id)
+
+
+    def __getitem__(self, index):
+        # handle the image redundancy
+        cur_vid = self.ann_id[index]
+
+        image = torch.Tensor(self.video_emb[cur_vid]['c3d_features'].value)
+        captions = self.jsondict[cur_vid]['sentences']
+        caption = ''
+        for cap in captions:
+            caption = caption+cap
+        vocab = self.vocab
+
+        # Convert caption (string) to word ids.
+        tokens = nltk.tokenize.word_tokenize(
+            caption.lower())
+        caption = []
+        caption.append(vocab('BOS'))
+        caption.extend([vocab(token) for token in tokens])
+        caption.append(vocab('EOS'))
+        target = torch.Tensor(caption)
+
+
+        #### baseline only ####
+        image = image.mean(0)
+
+        return image, target, index, cur_vid
+
+    def __len__(self):
+        return self.length
+
+
+def collate_fn(data):
+    """Build mini-batch tensors from a list of (image, caption) tuples.
+    Args:
+        data: list of (image, caption) tuple.
+            - image: torch tensor of shape (3, 256, 256).
+            - caption: torch tensor of shape (?); variable length.
+
+    Returns:
+        images: torch tensor of shape (batch_size, 3, 256, 256).
+        targets: torch tensor of shape (batch_size, padded_length).
+        lengths: list; valid length for each padded caption.
+    """
+    # Sort a data list by caption length
+    data.sort(key=lambda x: len(x[1]), reverse=True)
+    images, captions, ids, img_ids = zip(*data)
+
+    # Merge images (convert tuple of 3D tensor to 4D tensor)
+    images = torch.stack(images, 0)
+
+    # Merget captions (convert tuple of 1D tensor to 2D tensor)
+    lengths = [len(cap) for cap in captions]
+    targets = torch.zeros(len(captions), max(lengths)).long()
+    for i, cap in enumerate(captions):
+        end = lengths[i]
+        targets[i, :end] = cap[:end]
+
+    return images, targets, lengths, ids
+
+
+def get_precomp_loader(data_path, data_split, vocab, opt, batch_size=100,
+                       shuffle=True, num_workers=2):
+    """Returns torch.utils.data.DataLoader for custom coco dataset."""
+    dset = PrecompDataset(data_path, data_split, vocab)
+
+    data_loader = torch.utils.data.DataLoader(dataset=dset,
+                                              batch_size=batch_size,
+                                              shuffle=shuffle,
+                                              pin_memory=True,
+                                              collate_fn=collate_fn)
+    return data_loader
+
+
+def get_loaders(data_name, vocab, batch_size, workers, opt):
+    dpath = os.path.join(opt.data_path, data_name)
+    if opt.data_name.endswith('_precomp'):
+        train_loader = get_precomp_loader(dpath, 'train', vocab, opt,
+                                          batch_size, True, workers)
+        val_loader = get_precomp_loader(dpath, 'val_1', vocab, opt,
+                                        batch_size, False, workers)
+    return train_loader, val_loader
+
+
+def get_test_loader(split_name, data_name, vocab, batch_size,
+                    workers, opt):
+    dpath = os.path.join(opt.data_path, data_name)
+    if opt.data_name.endswith('_precomp'):
+        test_loader = get_precomp_loader(dpath, split_name, vocab, opt,
+                                         batch_size, False, workers)
+    return test_loader
---
+++ debug/debug_notebook_alg3/model.py
@@ -0,0 +1,320 @@
+import torch
+import torch.nn as nn
+import torch.nn.init
+import torchvision.models as models
+from torch.autograd import Variable
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
+import torch.backends.cudnn as cudnn
+from torch.nn.utils.clip_grad import clip_grad_norm
+import numpy as np
+from collections import OrderedDict
+from IPython import embed
+
+
+def l2norm(X):
+    """L2-normalize columns of X
+    """
+    norm = torch.pow(X, 2).sum(dim=1, keepdim=True).sqrt()
+    X = torch.div(X, norm)
+    return X
+
+
+def EncoderImage(data_name, img_dim, embed_size, finetune=False,
+                 cnn_type='vgg19', use_abs=False, no_imgnorm=False):
+    """A wrapper to image encoders. Chooses between an encoder that uses
+    precomputed image features, `EncoderImagePrecomp`, or an encoder that
+    computes image features on the fly `EncoderImageFull`.
+    """
+    if data_name.endswith('_precomp'):
+        img_enc = EncoderImagePrecomp(
+            img_dim, embed_size, use_abs, no_imgnorm)
+
+    return img_enc
+
+
+class EncoderImagePrecomp(nn.Module):
+
+    def __init__(self, img_dim, embed_size, use_abs=False, no_imgnorm=False):
+        super(EncoderImagePrecomp, self).__init__()
+        self.embed_size = embed_size
+        self.no_imgnorm = no_imgnorm
+        self.use_abs = use_abs
+
+        self.fc = nn.Linear(img_dim, embed_size)
+
+        self.init_weights()
+
+    def init_weights(self):
+        """Xavier initialization for the fully connected layer
+        """
+        r = np.sqrt(6.) / np.sqrt(self.fc.in_features +
+                                  self.fc.out_features)
+        self.fc.weight.data.uniform_(-r, r)
+        self.fc.bias.data.fill_(0)
+
+    def forward(self, images):
+        """Extract image feature vectors."""
+        # assuming that the precomputed features are already l2-normalized
+
+        images = l2norm(images)
+
+        features = self.fc(images)
+
+        # normalize in the joint embedding space
+        if not self.no_imgnorm:
+            features = l2norm(features)
+
+        # take the absolute value of embedding (used in order embeddings)
+        if self.use_abs:
+            features = torch.abs(features)
+
+        return features
+
+    def load_state_dict(self, state_dict):
+        """Copies parameters. overwritting the default one to
+        accept state_dict from Full model
+        """
+        own_state = self.state_dict()
+        new_state = OrderedDict()
+        for name, param in state_dict.items():
+            if name in own_state:
+                new_state[name] = param
+
+        super(EncoderImagePrecomp, self).load_state_dict(new_state)
+
+
+# tutorials/08 - Language Model
+# RNN Based Language Model
+class EncoderText(nn.Module):
+
+    def __init__(self, vocab_size, word_dim, embed_size, num_layers,
+                 use_abs=False):
+        super(EncoderText, self).__init__()
+        self.use_abs = use_abs
+        self.embed_size = embed_size
+
+        # word embedding
+        self.embed = nn.Embedding(vocab_size, word_dim)
+
+        # caption embedding
+        self.rnn = nn.GRU(word_dim, embed_size, num_layers, batch_first=True)
+
+        self.init_weights()
+
+    def init_weights(self):
+#        embed()
+#        self.embed.weight.data.uniform_(-0.1, 0.1)
+        coco_glove = torch.from_numpy(np.load('anet_precomp_w2v.npz')['arr_0'].astype(float)).float()
+        self.embed.weight.data = coco_glove
+
+    def forward(self, x, lengths):
+        """Handles variable size captions
+        """
+        # Embed word ids to vectors
+        x = self.embed(x)
+        packed = pack_padded_sequence(x, lengths, batch_first=True)
+
+        # Forward propagate RNN
+        out, _ = self.rnn(packed)
+
+        # Reshape *final* output to (batch_size, hidden_size)
+        padded = pad_packed_sequence(out, batch_first=True)
+        I = torch.LongTensor(lengths).view(-1, 1, 1)
+        I = Variable(I.expand(x.size(0), 1, self.embed_size)-1).cuda()
+        out = torch.gather(padded[0], 1, I).squeeze(1)
+
+        # normalization in the joint embedding space
+        out = l2norm(out)
+
+        # take absolute value, used by order embeddings
+        if self.use_abs:
+            out = torch.abs(out)
+
+        return out
+
+
+def cosine_sim(im, s):
+    """Cosine similarity between all the image and sentence pairs
+    """
+    return im.mm(s.t())
+
+
+def order_sim(im, s):
+    """Order embeddings similarity measure $max(0, s-im)$
+    """
+    YmX = (s.unsqueeze(1).expand(s.size(0), im.size(0), s.size(1))
+           - im.unsqueeze(0).expand(s.size(0), im.size(0), s.size(1)))
+    score = -YmX.clamp(min=0).pow(2).sum(2).sqrt().t()
+    return score
+
+
+class ContrastiveLoss(nn.Module):
+    """
+    Compute contrastive loss
+    """
+
+    def __init__(self, margin=0, measure=False, max_violation=False):
+        super(ContrastiveLoss, self).__init__()
+        self.margin = margin
+        if measure == 'order':
+            self.sim = order_sim
+        else:
+            self.sim = cosine_sim
+
+        self.max_violation = max_violation
+
+    def forward(self, im, s):
+        # compute image-sentence score matrix
+        scores = self.sim(im, s)
+        diagonal = scores.diag().view(im.size(0), 1)
+        d1 = diagonal.expand_as(scores)
+        d2 = diagonal.t().expand_as(scores)
+
+        # compare every diagonal score to scores in its column
+        # caption retrieval
+        cost_s = (self.margin + scores - d1).clamp(min=0)
+        # compare every diagonal score to scores in its row
+        # image retrieval
+        cost_im = (self.margin + scores - d2).clamp(min=0)
+
+        # clear diagonals
+        mask = torch.eye(scores.size(0)) > .5
+        I = Variable(mask)
+        if torch.cuda.is_available():
+            I = I.cuda()
+        cost_s = cost_s.masked_fill_(I, 0)
+        cost_im = cost_im.masked_fill_(I, 0)
+
+        # keep the maximum violating negative for each query
+        if self.max_violation:
+            cost_s = cost_s.max(1)[0]
+            cost_im = cost_im.max(0)[0]
+
+        return cost_s.sum() + cost_im.sum()
+
+
+class VSE(object):
+    """
+    rkiros/uvs model
+    """
+
+    def __init__(self, opt):
+        # tutorials/09 - Image Captioning
+        # Build Models
+        self.grad_clip = opt.grad_clip
+        self.img_enc = EncoderImage(opt.data_name, opt.img_dim, opt.embed_size,
+                                    use_abs=opt.use_abs,
+                                    no_imgnorm=opt.no_imgnorm)
+        self.txt_enc = EncoderText(opt.vocab_size, opt.word_dim,
+                                   opt.embed_size, opt.num_layers,
+                                   use_abs=opt.use_abs)
+        if torch.cuda.is_available():
+            self.img_enc.cuda()
+            self.txt_enc.cuda()
+            cudnn.benchmark = True
+
+        # Loss and Optimizer
+        self.criterion = ContrastiveLoss(margin=opt.margin,
+                                         measure=opt.measure,
+                                         max_violation=opt.max_violation)
+        params = list(self.txt_enc.parameters())
+        params += list(self.img_enc.fc.parameters())
+        self.params = params
+
+        self.optimizer = torch.optim.Adam(params, lr=opt.learning_rate)
+
+        self.Eiters = 0
+
+    def state_dict(self):
+        state_dict = [self.img_enc.state_dict(), self.txt_enc.state_dict()]
+        return state_dict
+
+    def load_state_dict(self, state_dict):
+        self.img_enc.load_state_dict(state_dict[0])
+        self.txt_enc.load_state_dict(state_dict[1])
+
+    def train_start(self):
+        """switch to train mode
+        """
+        self.img_enc.train()
+        self.txt_enc.train()
+
+    def val_start(self):
+        """switch to evaluate mode
+        """
+        self.img_enc.eval()
+        self.txt_enc.eval()
+
+    def forward_emb(self, video_whole, video_part, captions_whole, captions_part, lengths_whole, lengths_part, volatile=False):
+        """Compute the image and caption embeddings
+        """
+        # Set mini-batch dataset
+        video_whole = Variable(video_whole, volatile=volatile)
+        video_part = Variable(video_part, volatile=volatile)
+        captions_whole = Variable(captions_whole, volatile=volatile)
+        captions_part = Variable(captions_part, volatile=volatile)
+        if torch.cuda.is_available():
+            video_whole = video_whole.cuda()
+            video_part = video_part.cuda()
+            captions_whole = captions_whole.cuda()
+            captions_part = captions_part.cuda()
+
+        # Forward
+        vid_whole_emb = self.img_enc(video_whole)
+        vid_part_emb = self.img_enc(video_part)
+        vid_emb = torch.cat((vid_whole_emb, vid_part_emb), 1)
+
+        lengths_whole_total = [(l, i) for i,l in enumerate(lengths_whole)]
+        lengths_whole_total.sort(key=lambda x: x[0], reverse=True)
+        lengths_whole_sort = [x[0] for x in lengths_whole_total]
+        lengths_whole_sort_id = [x[1] for x in lengths_whole_total]
+        captions_whole_sort = captions_whole[lengths_whole_sort_id, :]
+
+        cap_whole_emb = self.txt_enc(captions_whole_sort, lengths_whole_sort)
+
+        cap_whole_emb = cap_whole_emb[np.argsort(lengths_whole_sort_id).tolist(), :]
+
+        lengths_part_total = [(l, i) for i,l in enumerate(lengths_part)]
+        lengths_part_total.sort(key=lambda x: x[0], reverse=True)
+        lengths_part_sort = [x[0] for x in lengths_part_total]
+        lengths_part_sort_id = [x[1] for x in lengths_part_total]
+        captions_part_sort = captions_part[lengths_part_sort_id, :]
+
+        cap_part_emb = self.txt_enc(captions_part_sort, lengths_part_sort)
+
+        cap_part_emb = cap_part_emb[np.argsort(lengths_part_sort_id).tolist(), :]
+
+
+#        cap_part_emb = self.txt_enc(captions_part, lengths_part)
+        cap_emb = torch.cat((cap_whole_emb, cap_part_emb), 1)
+
+        vid_emb = l2norm(vid_emb)
+        cap_emb = l2norm(cap_emb)
+        return vid_emb, cap_emb
+
+    def forward_loss(self, img_emb, cap_emb, **kwargs):
+        """Compute the loss given pairs of image and caption embeddings
+        """
+        loss = self.criterion(img_emb, cap_emb)
+        self.logger.update('Le', loss.data[0], img_emb.size(0))
+        return loss
+
+    def train_emb(self, video_whole, video_part, captions_whole, captions_part, lengths_whole, lengths_part, ids=None, *args):
+        """One training step given images and captions.
+        """
+        self.Eiters += 1
+        self.logger.update('Eit', self.Eiters)
+        self.logger.update('lr', self.optimizer.param_groups[0]['lr'])
+
+        # compute the embeddings
+        img_emb, cap_emb = self.forward_emb(video_whole, video_part, captions_whole, captions_part, lengths_whole, lengths_part)
+
+        # measure accuracy and record loss
+        self.optimizer.zero_grad()
+        loss = self.forward_loss(img_emb, cap_emb)
+
+        # compute gradient and do SGD step
+        loss.backward()
+        if self.grad_clip > 0:
+            clip_grad_norm(self.params, self.grad_clip)
+        self.optimizer.step()
